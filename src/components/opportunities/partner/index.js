f38b626b0f44683d6973e54207500433836c9d87
sdbradley <scott.bradley@daveramsey.com>
2018-02-24 21:25:43 -0600

    [O1-52] view feedback on opp detail

--- src/components/opportunities/partner/index.js
+++ src/components/opportunities/partner/index.js
@@ -1,1 +1,2 @@
 export { default } from './PartnerOpportunities'; // eslint-disable-line
+import './PartnerOpportunities.scss';
--- src/reducers/opportunities.js
+++ src/reducers/opportunities.js
@@ -1,5 +1,6 @@
 import { FETCHING_OPPORTUNITIES, FETCH_OPPORTUNITIES_SUCCESS, FETCHING_OPPORTUNITY, FETCH_OPPORTUNITY_SUCCESS } from 'actions/opportunities';
 import { RECEIVED_NORMAL_API_RESPONSE } from 'actions/api';
+import { LEAVE_FEEDBACK_SUCCESS } from 'actions/opportunities';
 import EntityCollection from 'util/EntityCollection';
 import { List } from 'immutable';
 
@@ -15,6 +16,12 @@ const opportunities = (list = INITIAL_STATE, action) => {
     case FETCHING_OPPORTUNITY:
     case FETCH_OPPORTUNITY_SUCCESS:
       return list.setMeta('fetched', list.getMeta('fetched').push(action.key));
+    case LEAVE_FEEDBACK_SUCCESS:
+      if(action.data) {
+        let o = list.find(action.data.opportunity_id);
+        o.feedback.push(action.data);
+        return list.save(o);
+      }
     case RECEIVED_NORMAL_API_RESPONSE:
       if (action.payload && action.payload.opportunities) {
         return list.saveAll(action.payload.opportunities);
