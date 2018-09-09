79bc3fd45edf54a18f6b9fc5bf7b60b1fc089141
sdbradley <scott.bradley@daveramsey.com>
2018-09-09 17:55:33 -0500

    [O1-167] fixed data clearing issue

--- src/components/campaigns/dashboard/CampaignDashboard.js
+++ src/components/campaigns/dashboard/CampaignDashboard.js
@@ -1,45 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import FetchOpportunities from 'containers/fetchers/fetch_opportunities';
-import FetchCampaign from 'containers/fetchers/fetch_campaign';
-import FetchStatistics from 'containers/fetchers/fetch_statistics';
-import FetchCampaignNews from 'containers/fetchers/fetch_campaign_news';
-import Widget from 'components/widget';
-import Link from 'components/link';
-import Opportunities from 'components/opportunities';
-import Campaigns from 'components/campaigns';
-import Statistics from 'components/statistics';
-import CampaignNews from 'components/campaign_news';
-import { APP_ROOT } from 'constants';
-import { downloadCSV } from 'actions/campaigns'
-import classNames from 'classnames'
-import Button from 'components/shared/button/button';
-import { setCampaign } from 'actions/navigation';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import FetchOpportunities from "containers/fetchers/fetch_opportunities";
+import FetchCampaign from "containers/fetchers/fetch_campaign";
+import FetchStatistics from "containers/fetchers/fetch_statistics";
+import FetchCampaignNews from "containers/fetchers/fetch_campaign_news";
+import Widget from "components/widget";
+import Link from "components/link";
+import Opportunities from "components/opportunities";
+import Campaigns from "components/campaigns";
+import Statistics from "components/statistics";
+import CampaignNews from "components/campaign_news";
+import { APP_ROOT } from "constants";
+import { downloadCSV } from "actions/campaigns";
+import classNames from "classnames";
+import Button from "components/shared/button/button";
+import { setCampaign } from "actions/navigation";
 
 class CampaignDashboard extends Component {
-
   constructor(props) {
-    super(props)
-    this.handleDownload = this.handleDownload.bind(this)
+    super(props);
+    this.handleDownload = this.handleDownload.bind(this);
     this.state = {
       downloading: false
-    }
+    };
   }
 
   componentDidMount() {
-    if(this.props.campaign) {
+    if (this.props.campaign) {
       this.props.dispatch(setCampaign(this.props.campaign.id));
     }
   }
 
   handleDownload() {
-    if (this.state.downloading || !this.props.campaign) return
+    if (this.state.downloading || !this.props.campaign) return;
     this.setState({ downloading: true }, () =>
       this.props
         .downloadCSV(this.props.campaign.id)
         .catch(e => console.error(e))
         .then(() => this.setState({ downloading: false }))
-    )
+    );
   }
 
   render() {
@@ -48,7 +47,9 @@ class CampaignDashboard extends Component {
         <div>
           <div className="Widget-full">
             <div className="Widget-half">
-              <h1 className="Admin-title">{this.props.campaign && this.props.campaign.name}</h1>
+              <h1 className="Admin-title">
+                {this.props.campaign && this.props.campaign.name}
+              </h1>
             </div>
           </div>
           <div className="Widget-full">
@@ -64,21 +65,29 @@ class CampaignDashboard extends Component {
             </div>
           </div>
           <div className="Widget-full">
-            <Widget title="Upcoming Opportunities">
-              <Opportunities data={this.props.opportunities} />
-              {this.renderDownloadLink()}
-            </Widget>
+            <FetchOpportunities campaignId={this.props.campaignId}>
+              <Widget title="Upcoming Opportunities">
+                <Opportunities data={this.props.opportunities} />
+                {this.renderDownloadLink()}
+              </Widget>
+            </FetchOpportunities>
           </div>
         </div>
       </FetchCampaign>
-    )
+    );
   }
 
   renderDownloadLink() {
     if (this.props.campaign) {
-      return <Button className='Gradebook-footerButton' disabled={this.state.downloading} onClick={this.handleDownload}>
-        Export to CSV
-      </Button>
+      return (
+        <Button
+          className="Gradebook-footerButton"
+          disabled={this.state.downloading}
+          onClick={this.handleDownload}
+        >
+          Export to CSV
+        </Button>
+      );
     }
   }
 }
@@ -86,11 +95,14 @@ class CampaignDashboard extends Component {
 export default connect(
   (state, props) => {
     let user = state.authentication.user;
-    let campaign = state.campaigns.findOneWhere(o => o.id===props.campaignId);
-    let opportunities = state.opportunities.findWhere(o => o.campaign_id===props.campaignId);
+    let campaign = state.campaigns.findOneWhere(o => o.id === props.campaignId);
+    let opportunities = state.opportunities.findWhere(
+      o => o.campaign_id === props.campaignId
+    );
     return {
       user: user,
       campaign: campaign,
+      campaignId: props.campaignId,
       campaign_news: state.campaign_news.all(),
       opportunities: opportunities,
       start_date: state.navigation && state.navigation.startDate,
