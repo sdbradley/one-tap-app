08bf8d0308505eff227b1be6d2510506a8ee6838
sdbradley <scott.bradley@daveramsey.com>
2018-09-17 20:47:55 -0500

    [O1-169] set password on create user

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,24 +1,24 @@
-import API from 'util/API';
-import { receivedNormalAPIResponse } from 'actions/api';
-import { encodePlus } from 'util/funcs'
+import API from "util/API";
+import { receivedNormalAPIResponse } from "actions/api";
+import { encodePlus } from "util/funcs";
 
-export const FETCHING_USERS = 'FETCHING_USERS';
-export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
-export const USER_CREATE_SUCCESS = 'USER_CREATE_SUCCESS';
-export const USER_UPDATE_SUCCESS = 'USER_UPDATE_SUCCESS';
-export const DELETE_USER_SUCCESS = 'DELETE_USER_SUCCESS';
-export const SET_PASSWORD_SUCCESS = 'SET_PASSWORD_SUCCESS'
-export const RECEIVED_SEARCH_RESULTS = 'RECEIVED_SEARCH_RESULTS'
-export const SEARCH_USERS_START = 'SEARCH_USERS_START'
+export const FETCHING_USERS = "FETCHING_USERS";
+export const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+export const USER_CREATE_SUCCESS = "USER_CREATE_SUCCESS";
+export const USER_UPDATE_SUCCESS = "USER_UPDATE_SUCCESS";
+export const DELETE_USER_SUCCESS = "DELETE_USER_SUCCESS";
+export const SET_PASSWORD_SUCCESS = "SET_PASSWORD_SUCCESS";
+export const RECEIVED_SEARCH_RESULTS = "RECEIVED_SEARCH_RESULTS";
+export const SEARCH_USERS_START = "SEARCH_USERS_START";
 
 export function fetchUsers() {
   return (dispatch, getState) => {
     dispatch(fetchingUsers());
     let url = `admin/users`;
     return API.get(url)
-      .then((res) => dispatch(receivedNormalAPIResponse(res)))
+      .then(res => dispatch(receivedNormalAPIResponse(res)))
       .then(() => dispatch(fetchUsersSuccess()));
-  }
+  };
 }
 
 export function fetchingUsers() {
@@ -38,29 +38,45 @@ export function createUser({
   last_name,
   email_address,
   user_name,
+  password,
   account_id
 }) {
-  if (!first_name || !last_name || !email_address || !user_name || !account_id) {
-    throw new Error('All fields are required');
+  if (
+    !first_name ||
+    !last_name ||
+    !email_address ||
+    !password ||
+    !user_name ||
+    !account_id
+  ) {
+    throw new Error("All fields are required");
   }
 
-  return (dispatch, getState) => API.admin.post(
-    `users`,
-    {
-      first_name,
-      last_name,
-      email_address,
-      user_name,
-      account_id
-    }
-  )
-  .then(res => dispatch({ type: USER_CREATE_SUCCESS, user: [{ 
-    first_name, 
-    last_name, 
-    email_address, 
-    user_name, 
-    account_id 
-  }] }));
+  return (dispatch, getState) =>
+    API.admin
+      .post(`users`, {
+        first_name,
+        last_name,
+        email_address,
+        user_name,
+        password,
+        account_id
+      })
+      .then(res =>
+        dispatch({
+          type: USER_CREATE_SUCCESS,
+          user: [
+            {
+              first_name,
+              last_name,
+              email_address,
+              user_name,
+              password,
+              account_id
+            }
+          ]
+        })
+      );
 }
 
 export function updateUser({
@@ -71,31 +87,46 @@ export function updateUser({
   user_name,
   account_id
 }) {
-  if (!first_name || !last_name || !email_address || !user_name || !account_id) {
-    throw new Error('All fields are required');
+  if (
+    !first_name ||
+    !last_name ||
+    !email_address ||
+    !user_name ||
+    !account_id
+  ) {
+    throw new Error("All fields are required");
   }
 
-  return (dispatch, getState) => API.admin.put(
-    `users/${user_id}`,
-    {
-      first_name,
-      last_name,
-      email_address,
-      user_name,
-      account_id
-    })
-    .then(res => dispatch({ type: USER_UPDATE_SUCCESS, user: [{ 
-      first_name, 
-      last_name, 
-      email_address, 
-      user_name, 
-      account_id 
-    }] }));
+  return (dispatch, getState) =>
+    API.admin
+      .put(`users/${user_id}`, {
+        first_name,
+        last_name,
+        email_address,
+        user_name,
+        account_id
+      })
+      .then(res =>
+        dispatch({
+          type: USER_UPDATE_SUCCESS,
+          user: [
+            {
+              first_name,
+              last_name,
+              email_address,
+              user_name,
+              account_id
+            }
+          ]
+        })
+      );
 }
 
 export function deleteUser(user_id) {
-  return (dispatch, getState) => API.admin.delete(`users/${user_id}`)
-    .then(() => dispatch(deleteUserSuccess(user_id)));
+  return (dispatch, getState) =>
+    API.admin
+      .delete(`users/${user_id}`)
+      .then(() => dispatch(deleteUserSuccess(user_id)));
 }
 
 function deleteUserSuccess(userId) {
@@ -108,41 +139,41 @@ function deleteUserSuccess(userId) {
 export function setPassword({ id, email, password }) {
   return dispatch =>
     API.put(`/users/${id}`, {
-        email,
-        password
-      })
+      email,
+      password
+    })
       .then(res => {
-        dispatch({ type: SET_PASSWORD_SUCCESS })
-        dispatch(receivedNormalAPIResponse(res))
+        dispatch({ type: SET_PASSWORD_SUCCESS });
+        dispatch(receivedNormalAPIResponse(res));
       })
       .catch(err => {
         if (err.status === 422) {
-          throw new Error('Please meet the password criteria above.')
+          throw new Error("Please meet the password criteria above.");
         }
-        throw new Error('Server error. Try again later.')
-      })
+        throw new Error("Server error. Try again later.");
+      });
 }
 
 export function searchUsers(params) {
   if (params.search_term === undefined) {
-    throw new Error('Please include a search_term.')
+    throw new Error("Please include a search_term.");
   }
   if (params.search_term.length < 2) {
-    throw new Error('Must include at least 2 characters in your search')
+    throw new Error("Must include at least 2 characters in your search");
   }
-  params.search_term = encodePlus(params.search_term)
+  params.search_term = encodePlus(params.search_term);
   return dispatch => {
-    dispatch({ type: SEARCH_USERS_START })
+    dispatch({ type: SEARCH_USERS_START });
     API.admin.get(`/users?search_term=${params.search_term}`).then(res => {
       dispatch({
         type: RECEIVED_SEARCH_RESULTS,
         ids: (res.users || []).map(user => user.id)
-      })
-      dispatch(receivedNormalAPIResponse(res))
-    })
-  }
+      });
+      dispatch(receivedNormalAPIResponse(res));
+    });
+  };
 }
 
 function _fetch(params) {
-  return API.admin.get(`/users`, { params })
+  return API.admin.get(`/users`, { params });
 }
--- src/components/modals/edit_user/EditUser.js
+++ src/components/modals/edit_user/EditUser.js
@@ -1,69 +1,83 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Modal from 'containers/modal';
-import Field from 'components/field';
-import Button from 'components/shared/button';
-import Form from 'components/Form';
-import FetchAccounts from 'containers/fetchers/fetch_accounts';
+import React from "react";
+import { connect } from "react-redux";
+import Modal from "containers/modal";
+import Field from "components/field";
+import Button from "components/shared/button";
+import Form from "components/Form";
+import FetchAccounts from "containers/fetchers/fetch_accounts";
 
 import {
   createUser,
   USER_CREATE_SUCCESS,
   updateUser,
   USER_UPDATE_SUCCESS
-} from 'actions/users';
+} from "actions/users";
 
-function EditUser({
-  user,
-  accounts,
-  updateUser,
-  createUser
-} = {}) {
+function EditUser({ user, accounts, updateUser, createUser } = {}) {
   return (
     <FetchAccounts onlyOTPClients={true}>
-        <Modal closeAction={user ? USER_UPDATE_SUCCESS : USER_CREATE_SUCCESS}>
-            <div className='EditUser'>
-            <Form className='EditUser-form' onSubmit={user ? updateUser : createUser}>
-                <h1 className='EditUser-title'>{user ? 'Edit' : 'Add'} User</h1>
-                { user ? <Field type='hidden' name='user_id' value={user.id} /> : null}
-                <Field
-                type='text'
-                name='first_name'
-                label='First name'
-                value={user && user.first_name}
-                />
-                <Field
-                type='text'
-                name='last_name'
-                label='Last name'
-                value={user && user.last_name}
-                />
-                <Field
-                type='text'
-                name='email_address'
-                label='Email'
-                value={user && user.email_address}
-                />
-                <Field
-                type='text'
-                name='user_name'
-                label='Username'
-                value={user && user.user_name}
-                />
-                <Field
-                type='select'
-                label='Account'
-                name='account_id'
-                value={user && user.account_id}
-                >
-                {accounts.map(a => <option key={a.id} value={a.id} label={a.name}/>)}
-                </Field>
-                <Field type='checkbox' name='is_stakeholder' value={true}>Stakeholder</Field>
-                <Field type='checkbox' name='is_admin' value={true}>Admin</Field>
-                <Button full submit>{user ? 'Save' : 'Add'}</Button>
-            </Form>
-            </div>
-        </Modal>
+      <Modal closeAction={user ? USER_UPDATE_SUCCESS : USER_CREATE_SUCCESS}>
+        <div className="EditUser">
+          <Form
+            className="EditUser-form"
+            onSubmit={user ? updateUser : createUser}
+          >
+            <h1 className="EditUser-title">{user ? "Edit" : "Add"} User</h1>
+            {user ? (
+              <Field type="hidden" name="user_id" value={user.id} />
+            ) : null}
+            <Field
+              type="text"
+              name="first_name"
+              label="First name"
+              value={user && user.first_name}
+            />
+            <Field
+              type="text"
+              name="last_name"
+              label="Last name"
+              value={user && user.last_name}
+            />
+            <Field
+              type="text"
+              name="email_address"
+              label="Email"
+              value={user && user.email_address}
+            />
+            <Field
+              type="text"
+              name="user_name"
+              label="Username"
+              value={user && user.user_name}
+            />
+            <Field
+              type="password"
+              name="password"
+              label="Password"
+              value={user && user.password}
+            />
+            <Field
+              type="select"
+              label="Account"
+              name="account_id"
+              value={user && user.account_id}
+            >
+              {accounts.map(a => (
+                <option key={a.id} value={a.id} label={a.name} />
+              ))}
+            </Field>
+            <Field type="checkbox" name="is_stakeholder" value={true}>
+              Stakeholder
+            </Field>
+            <Field type="checkbox" name="is_admin" value={true}>
+              Admin
+            </Field>
+            <Button full submit>
+              {user ? "Save" : "Add"}
+            </Button>
+          </Form>
+        </div>
+      </Modal>
     </FetchAccounts>
   );
 }
